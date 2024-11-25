beb16488cf31c4c9f4a9619f17d2037a2790521c
Rémi Michel <remi.michel38@gmail.com>
2024-11-25 12:30:10 +0100

    feat(): test with hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard.jsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
