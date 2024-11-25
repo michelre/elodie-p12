bb041a7dc0395c7864d72ff9e5411dafe3cd9d6f
Rémi Michel <remi.michel38@gmail.com>
2024-11-25 12:21:40 +0100

    feat(): test deploy 6

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const BASE_URL = process.env.BASE_URL || '';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `${BASE_URL}/`,
 })
 
